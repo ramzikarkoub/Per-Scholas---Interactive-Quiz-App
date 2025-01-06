4e065517e9081d1979dd52f5d91177ae71465c32
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-01-06 15:02:04 -0500

    added score / score tracking / alert result

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import { quiz } from "./data.js";
-
+console.log(quiz.length);
 const questionContainer = document.querySelector(".question");
 const questionTitle = document.querySelector(".questionTitle");
 const ul = document.querySelector(".ul");
-
+let score = 0;
+let questionAnswered = 0;
 function runQuiz(quiz) {
   for (let i = 0; i < quiz.length; i++) {
     const element = quiz[i];
@@ -35,9 +36,24 @@ function runQuiz(quiz) {
         if (checkAnswer(ChosenAnswer, correctAnswer)) {
           console.log("Correct Answer");
           answer.classList.add("correct");
+          score++;
+          questionAnswered++;
+          console.log(score);
         } else {
           console.log("Incorrect Answer");
           answer.classList.add("false");
+          questionAnswered++;
+        }
+        console.log(questionAnswered);
+        if (questionAnswered == quiz.length) {
+          let result = (score / questionAnswered) * 100;
+          if (result >= 70) {
+            alert(`YOU WON!!! you answered ${result}% of the quiz correctly`);
+          } else if (result <= 70) {
+            alert(
+              `Oh, no! you answered ${result}% of the quiz correctly, try again`
+            );
+          }
         }
       });
     }
@@ -51,7 +67,6 @@ const disableOptions = (optionsList) => {
   const allOptions = optionsList.querySelectorAll("li");
   allOptions.forEach((el) => {
     el.style.pointerEvents = "none";
-    el.classList.add("disabled");
   });
 };
 
@@ -60,3 +75,4 @@ const checkAnswer = (ChosenAnswer, correctAnswer) => {
 };
 
 runQuiz(quiz);
+console.log(score);
